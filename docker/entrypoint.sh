3333a7dc683f6f3128fd00458fe6697a6e1d1365
aazsamir <sutikkukun@gmail.com>
2025-11-16 13:17:09 +0100

    add migrate to entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,5 +3,7 @@
 # Start cron in background
 supercronic /etc/tempest-cron &
 
+php /app/tempest migrate
+
 # Continue with the original entrypoint
 exec docker-php-entrypoint --config /etc/frankenphp/Caddyfile --adapter caddyfile
